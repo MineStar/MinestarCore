e83930008016a8b94eaa5e2401829fe4b772485d
GeMoschen <x-chris-x@arcor.de>
2012-03-01 17:13:24 +0100

    - only call the update once

--- src/main/java/de/minestar/core/units/MinestarPlayer.java
+++ src/main/java/de/minestar/core/units/MinestarPlayer.java
@@ -67,9 +67,6 @@ public class MinestarPlayer {
 
         // LOAD DATA
         this.loadData();
-
-        // UPDATE THE BUKKITPLAYER
-        this.updateBukkitPlayer();
     }
 
     public String getPlayerName() {
